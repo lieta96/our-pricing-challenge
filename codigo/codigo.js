0096b0d99d4774c7756dd49dd4afb82d26e0d650
Julieta Garcia <julieta.garcia96@gmail.com>
2021-02-05 16:52:22 -0300

    optimicé el código js, armando arrays con la información necesaria, y recorriéndolos con un for

--- codigo/codigo.js
+++ codigo/codigo.js
@@ -1,27 +1,37 @@
-function annualyPrices(){
-    let newBasicPrice=document.getElementById("basic-price");
-    newBasicPrice.innerHTML="&dollar;199.99";
-    let newProfessionalPrice=document.getElementById("professional-price");
-    newProfessionalPrice.innerHTML="&dollar;249.99";
-    let newMasterPrice=document.getElementById("master-price");
-    newMasterPrice.innerHTML="&dollar;399.99";
-}; /!--esto funciona--/
-function monthlyPrices(){
-    let newBasicPrice=document.getElementById("basic-price");
-    newBasicPrice.innerHTML="&dollar;19.99";
-    let newProfessionalPrice=document.getElementById("professional-price");
-    newProfessionalPrice.innerHTML="&dollar;24.99";
-    let newMasterPrice=document.getElementById("master-price");
-    newMasterPrice.innerHTML="&dollar;39.99";
+// Arrays that contains the annualy/monthly prices
+let annualyPrices = ["&dollar;199.99","&dollar;249.99","&dollar;399.99"]
+let monthlyPrices=["&dollar;19.99","&dollar;24.99","&dollar;39.99"]
+
+//Get the elements that contains the prices
+let basicPrice=document.getElementById("basic-price");
+let professionalPrice=document.getElementById("professional-price");
+let masterPrice=document.getElementById("master-price");
+
+// Array that contains the diferents options    
+let card=[basicPrice, masterPrice,professionalPrice ];
+
+// Set annualy/monthly Prices using the array card
+function setAnnualyPrices(){
+  for (let index = 0; index < annualyPrices.length; index++) {
+    card[index].innerHTML=annualyPrices[index];    
+  }  
+};
+function setMonthlyPrices(){
+  for (let index = 0; index < monthlyPrices.length; index++) {
+    card[index].innerHTML=monthlyPrices[index];    
+  }
 };
 
+//Get the switch button
 let switchLabel= document.getElementById("switch-label");
 
+// Change the prices acoording to the switch button
 switchLabel.addEventListener('change', function() {
   if (this.checked) {
-    monthlyPrices();
+    setMonthlyPrices();
   } else {
-    
-    annualyPrices();
+    setAnnualyPrices();
   }
 });
+
+setMonthlyPrices();
